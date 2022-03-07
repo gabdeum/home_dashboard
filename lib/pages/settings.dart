a2169d1c7f8cef3e0ab022622a49eb390d5647cf
Gabriel Demange <gabriel.demange@gmail.com>
2022-03-07 06:33:45 -0500

    modified AndroidManifest.xml + added geolocation + reverse geoCoding logic to button

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:home_dashboard/services/geoLocator.dart';
 import 'package:home_dashboard/services/formatClasses.dart';
+import 'package:geolocator/geolocator.dart';
 
 class Settings extends StatefulWidget {
 
@@ -83,40 +84,54 @@ class _SettingsState extends State<Settings> {
                   children: [
                     Text('Weather & Clock', style: MyTextStyle().largeDark,),
                     const SizedBox(height: 20.0,),
-                    Container(
-                      constraints: const BoxConstraints(maxWidth: 400),
-                      child: TextField(
-                        controller: addressController,
-                        decoration: InputDecoration(
-                            isDense: true,
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(40.0),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            controller: addressController,
+                            decoration: InputDecoration(
+                                isDense: true,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(40.0),
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(40.0),
+                                    borderSide: BorderSide(color: MyColors().darkColor1, width: 2.0)
+                                ),
+                                hintText: '125 rue Lamarck, Paris',
+                                hintStyle: MyTextStyle().mediumDark,
+                                labelText: 'Enter address manually',
+                                labelStyle: MyTextStyle().mediumDark,
+                                prefixIcon: Icon(Icons.location_pin, color: MyColors().darkColor1,),
+                                suffixIcon: IconButton(
+                                  icon: Icon(Icons.close, color: MyColors().darkColor1,),
+                                  onPressed: () {
+                                    addressController.clear();
+                                    _newLoc = {};
+                                    setState(() {});
+                                  },
+                                )
                             ),
-                            enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(40.0),
-                                borderSide: BorderSide(color: MyColors().darkColor1, width: 2.0)
-                            ),
-                            hintText: '125 rue Lamarck, Paris',
-                            hintStyle: MyTextStyle().mediumDark,
-                            labelText: 'Address',
-                            labelStyle: MyTextStyle().mediumDark,
-                            prefixIcon: Icon(Icons.location_pin, color: MyColors().darkColor1,),
-                            suffixIcon: IconButton(
-                              icon: Icon(Icons.close, color: MyColors().darkColor1,),
-                              onPressed: () {
-                                addressController.clear();
-                                _newLoc = {};
-                                setState(() {});
-                              },
-                            )
+                            keyboardType: TextInputType.streetAddress,
+                            textInputAction: TextInputAction.search,
+                            onSubmitted: (address) async {
+                              _newLoc = await GeoLoc(address: address.toString()).getLoc();
+                              setState(() {});
+                            },
+                          ),
                         ),
-                        keyboardType: TextInputType.streetAddress,
-                        textInputAction: TextInputAction.search,
-                        onSubmitted: (address) async {
-                          _newLoc = await GeoLoc(address: address.toString()).getLoc();
-                          setState(() {});
-                        },
-                      ),
+                        const SizedBox(width: 10.0,),
+                        FloatingActionButton(
+                          onPressed: () async {
+                            Position newPosition = await _determinePosition();
+                            _newLoc = await GeoLoc(latitude: newPosition.latitude, longitude: newPosition.longitude).getLocFromLatLong();
+                            setState(() {});
+                          },
+                          child: Icon(Icons.location_searching, color: MyColors().textColor,),
+                          backgroundColor: MyColors().color1,
+                          elevation: 0.0,
+                        )
+                      ],
                     ),
                     const SizedBox(height: 15.0,),
                     _newLoc.isEmpty ? Container(width: 0,) : ListTile(
@@ -135,4 +150,43 @@ class _SettingsState extends State<Settings> {
   }
 }
 
+/// Determine the current position of the device.
+///
+/// When the location services are not enabled or permissions
+/// are denied the `Future` will return an error.
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
 
+  // Test if location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // Location services are not enabled don't continue
+    // accessing the position and request users of the
+    // App to enable the location services.
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Permissions are denied, next time you could try
+      // requesting permissions again (this is also where
+      // Android's shouldShowRequestPermissionRationale
+      // returned true. According to Android guidelines
+      // your App should show an explanatory UI now.
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    // Permissions are denied forever, handle appropriately.
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
+
+  // When we reach here, permissions are granted and we can
+  // continue accessing the position of the device.
+  return await Geolocator.getCurrentPosition();
+}
--- lib/services/geoLocator.dart
+++ lib/services/geoLocator.dart
@@ -5,14 +5,16 @@ import 'package:flutter/material.dart';
 class GeoLoc {
 
   Map location = {};
-  final String address;
+  final String? address;
+  final double? latitude;
+  final double? longitude;
   final String key = 'AIzaSyCBsCjKGavUAdWN6hTCVQWBH81ARmNqBUI';
 
-  GeoLoc({required this.address});
+  GeoLoc({this.address, this.latitude, this.longitude});
 
-  getLoc() async {
+  Future<Map> getLoc() async {
 
-    String _address = address.replaceAll(' ', '%20');
+    String? _address = address?.replaceAll(' ', '%20');
     String _url = 'https://maps.googleapis.com/maps/api/geocode/json?address=$_address&key=$key';
     Response _response = await get(Uri.parse(_url));
 
@@ -29,4 +31,23 @@ class GeoLoc {
     return location;
 
   }
+
+  Future<Map> getLocFromLatLong() async {
+
+    String _url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=$latitude,$longitude&key=$key';
+    Response _response = await get(Uri.parse(_url));
+    final Map _data = const JsonDecoder().convert(_response.body);
+    final List _dataList =  _data['results'] as List;
+
+    location = {
+      'leading' : _dataList.isEmpty ? Icons.not_listed_location : Icons.check_circle,
+      'fullName' : _dataList.isEmpty ? "Couldn't find exact location, please precise" : _dataList[0]['formatted_address'],
+      'lat' : _dataList.isEmpty ? '' : _dataList[0]['geometry']['location']['lat'],
+      'lon' : _dataList.isEmpty ? '' : _dataList[0]['geometry']['location']['lng']
+    };
+
+    return location;
+
+  }
+
 }
